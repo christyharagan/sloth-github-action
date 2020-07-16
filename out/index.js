f1df68b7e96b728be9bfa0b2929e6c2efa18a77e
Christy Haragan <christy.haragan.github@gmail.com>
2020-07-16 11:04:01 +0100

    Rename main js file

--- out/index.js
+++ out/index.js
@@ -23,10 +23,11 @@ const npm_install_1 = require("npm-install");
 const segment_sloth_1 = require("segment-sloth");
 const core = __importStar(require("@actions/core"));
 let access_token = core.getInput('segment-access-token');
+let work_slug = core.getInput('segment-workspace-slug');
 let work_id = core.getInput('segment-workspace-id');
 npm_install_1.npmInstallAction().then(() => {
     console.log('Ready to deploy function');
-    segment_sloth_1.deploy(false, access_token, work_id).then(() => {
+    segment_sloth_1.deploy(false, access_token, work_slug, work_id).then(() => {
         console.log('Function successfully deployed');
     }).catch(e => {
         console.error('Error deploying function:');
