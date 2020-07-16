8b28f8ad18cabbfd64dfdaf7d87f82cb636f6d16
Christy Haragan <christy.haragan.github@gmail.com>
2020-07-16 13:55:29 +0100

    Add node_modules

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import {npmInstallAction} from 'npm-install'
-import {deploy} from 'segment-sloth'
+// import {npmInstallAction} from 'npm-install'
+import { deploy } from 'segment-sloth'
 import * as core from '@actions/core'
 
 let access_token = core.getInput('segment-access-token')
 let work_slug = core.getInput('segment-workspace-slug')
 let work_id = core.getInput('segment-workspace-id')
 
-npmInstallAction().then(() => {
-  console.log('Ready to deploy function')
-  deploy(false, access_token, work_slug, work_id).then(() => {
-    console.log('Function successfully deployed')
-  }).catch(e => {
-    console.error('Error deploying function:')
-    console.error(e)
-  })
+// npmInstallAction().then(() => {
+// console.log('Ready to deploy function')
+deploy(false, access_token, work_slug, work_id).then(() => {
+  console.log('Function successfully deployed')
+}).catch(e => {
+  console.error('Error deploying function:')
+  console.error(e)
 })
+// })
