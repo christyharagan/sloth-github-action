f1cc11ff0372c5214afc7e1e610738e6b148bee7
Christy Haragan <christy.haragan.github@gmail.com>
2020-07-21 12:16:15 +0100

    Bump versions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// import {npmInstallAction} from 'npm-install'
 import { deploy } from 'segment-sloth'
 import * as core from '@actions/core'
 
@@ -6,12 +5,9 @@ let access_token = core.getInput('segment-access-token')
 let work_slug = core.getInput('segment-workspace-slug')
 let work_id = core.getInput('segment-workspace-id')
 
-// npmInstallAction().then(() => {
-// console.log('Ready to deploy function')
 deploy(false, access_token, work_slug, work_id).then(() => {
   console.log('Function successfully deployed')
 }).catch(e => {
   console.error('Error deploying function:')
   console.error(e)
 })
-// })
